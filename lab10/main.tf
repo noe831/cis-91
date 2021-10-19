8454d265e5c696c3966365d3a97ee59f23d4abf8
noe831 <liliharbottle@gmail.com>
2021-10-19 23:14:04 +0000

    Updates 3

---
+++ lab10/main.tf
@@ -0,0 +1,81 @@
+variable "credentials_file" { 
+  default = "../secrets/cis-91.key" 
+}
+
+variable "project" {
+  default = "phonic-arcana-324121"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  region  = var.region
+  zone    = var.zone 
+  project = var.project
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "cis91-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "cis91"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+
+  attached_disk {
+      source = "lab10-disk"
+      mode = "READ_WRITE"
+      device_name = "lab10-disk"
+  }
+}
+
+resource "google_compute_disk" "default" {
+  name  = "lab10-disk"
+  type  = "pd-standard"
+  labels = {
+    environment = "dev"
+  }
+  size = 100
+}
+
+resource "google_compute_firewall" "default-firewall" {
+  name = "default-firewall"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports = ["22", "80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+output "external-ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
