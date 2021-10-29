631453343eed77a66970d13e7fa1ee7141127279
noe831 <liliharbottle@gmail.com>
2021-10-29 05:43:24 +0000

    final draft v2

--- dokuwiki/dokuwiki-backup.sh
+++ dokuwiki/dokuwiki-backup.sh
@@ -4,6 +4,6 @@ TARGET="gs://backup-bucket-324121"
 
 tar_file=/tmp/dokuwiki-backup-$(date +%s).tar.gz
 tar -czf $tar_file /var/www/html 2>/dev/null 
-gsutil cp $tar_file $TARGET
+/snap/bin/gsutil cp $tar_file $TARGET
 rm -f $tar_file
 
